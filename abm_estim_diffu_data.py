8ba6828d796fe13bbae19314464ccd7c49833b14
uscxiaoyu <yx_402@usc.edu>
2019-01-01 23:07:49 +0800

    win, abm fitting

--- abm_estim_diffu_data.py
+++ abm_estim_diffu_data.py
@@ -13,21 +13,21 @@ class EstimateABM:
     num_conds = 2  # 构建网格的节点个数
     k = 6  # 平均前继数量
 
-    def __init__(self, s, intv_p=0.001, intv_q=0.005, G=nx.gnm_random_graph(10000, 30000)):
+    def __init__(self, s, intv_p=0.0005, intv_q=0.005, G=nx.gnm_random_graph(10000, 30000)):
         self.s = s
         self.s_len = len(s)
         self.intv_p = intv_p
         self.intv_q = intv_q
         self.G = G
-    
-    def r2(self,f_act):
+
+    def r2(self, f_act):
         f_act = np.array(f_act)
         tse = np.sum(np.square(self.s - f_act))
         mean_y = np.mean(self.s)
         ssl = np.sum(np.square(self.s - mean_y))
         R_2 = (ssl - tse) / ssl
         return R_2
-    
+
     def get_M(self, p, q):  # 获取对应扩散率曲线的最优潜在市场容量
         diffu = Diffuse(p, q, g=self.G, num_runs=self.s_len)
         s_estim = diffu.repete_diffuse()
@@ -39,20 +39,21 @@ class EstimateABM:
         sigma = -b/(2*a)
         m = sigma*self.G.number_of_nodes()
         return [mse, p, q, m], list(x*sigma)
-    
+
     def gener_grid(self, p, q):
-        temp = {(p - self.intv_p, q - self.intv_q), 
-                (p, q - self.intv_q), 
+        p, q = round(p, 5), round(q, 5)
+        temp = {(p - self.intv_p, q - self.intv_q),
+                (p, q - self.intv_q),
                 (p + self.intv_p, q - self.intv_q),
                 (p - self.intv_p, q),
-                (p, q),       
+                (p, q),
                 (p + self.intv_p, q),
-                (p - self.intv_p, q + self.intv_q), 
-                (p, q + self.intv_q), 
+                (p - self.intv_p, q + self.intv_q),
+                (p, q + self.intv_q),
                 (p + self.intv_p, q + self.intv_q)
                 }
         return temp
-    
+
     def gener_p0_q0(self):  # 生成初始搜索点(p0,q0)
         rgs = BassEstimate(self.s)
         P0, Q0 = rgs.optima_search()[1 : 3]  # SABM最优点（P0,Q0）
@@ -68,7 +69,7 @@ class EstimateABM:
                 rgs_1 = BassEstimate(s_estim_avr)
                 P, Q = rgs_1.optima_search()[1 : 3]
                 params_cont.append([[p, q], [P, Q]])
-        
+
         to_fit['p'] = [x[0][0] for x in params_cont]
         to_fit['q'] = [x[0][1] for x in params_cont]
         to_fit['P'] = [x[1][0] for x in params_cont]
@@ -80,25 +81,28 @@ class EstimateABM:
 
         p0 = result_p.params['P']*P0 + result_p.params['Q']*Q0
         q0 = result_q.params['P']*P0 + result_q.params['Q']*Q0
-        return p0, q0
-    
+        return round(p0, 5), round(q0, 5)   # 保留5位小数位，防止出错
+
     def solution_search(self, p0, q0):
         solution_list = []
         pq_set = self.gener_grid(p0, q0)
         pq_trace = [pq_set]  # 初始化(p, q)的搜索轨迹
         for p, q in pq_set:
-            p, q = round(p, 5), round(q, 5)  # 保留小数位，防止出错
-            solution = self.get_M(p ,q)
-            solution_list.append(solution) # ([mse, p, q, s_M], [扩散曲线])
+            solution = self.get_M(p, q)
+            solution_list.append(solution)  # ([mse, p, q, s_M], [扩散曲线])
 
         best_solution = sorted(solution_list)[:self.num_conds]  # 选取num_conds个候选点
+        i = 0
         while True:
+            i += 1
             pq_set2 = set()
             for z in best_solution:  # 可以取多个作为候选最优解
                 temp = self.gener_grid(z[0][1], z[0][2])
                 pq_set2.update(temp)
 
             new_points = pq_set2 - pq_set  # 集合减, 未包含在pd_set中的新(p, q)
+            print(f"第{i}轮, 新增点个数:{len(new_points)}")
+            print(pq_set)
             if len(new_points) == 0:
                 break
             else:
@@ -108,9 +112,9 @@ class EstimateABM:
                     solution = self.get_M(y[0], y[1])
                     solution_list2.append(solution)
 
-                best_solution = sorted(solution_list2, key=lambda x : x[0][0])[:self.num_conds]
+                best_solution = sorted(solution_list2, key=lambda x: x[0][0])[: self.num_conds]
                 opt_solution = best_solution[0]
-                opt_curve = opt_solution[1]
+                opt_curve = opt_solution[1]  # p, q, m
 
                 solution_list.extend(solution_list2)
                 pq_set.update(new_points)
@@ -119,13 +123,13 @@ class EstimateABM:
         R2 = self.r2(f_act)
         search_steps = len(pq_set)  # 搜索点的数量
         result = {'params': opt_solution[1:], 'fitness': R2,
-                  'best_curve': f_act, 'steps': search_steps, 'path': pq_trace}  # [p,q,m], 拟合曲线, 搜索步数, 搜索范围
+                  'best_curve': f_act, 'num_nodes': search_steps, 'path': pq_trace}  # [p,q,m], 拟合曲线, 搜索步数, 搜索范围
         return result
 
 
 if __name__ == '__main__':
-    data_set = {'room air conditioners':(np.arange(1949,1962),[96,195,238,380,1045,1230,1267,1828,1586,
-                                                               1673,1800,1580,1500]),
+    data_set = {'room air conditioners':(np.arange(1949, 1962), [96, 195, 238, 380, 1045, 1230, 1267, 1828, 1586,
+                                                               1673, 1800, 1580, 1500]),
             'color televisions':(np.arange(1963,1971),[747,1480,2646,5118,5777,5982,5962,4631]),
             'clothers dryers':(np.arange(1949,1962),[106,319,492,635,737,890,1397,1523,1294,1240,1425,1260,1236]),
             'ultrasound':(np.arange(1965,1979),[5,3,2,5,7,12,6,16,16,28,28,21,13,6]),
@@ -145,7 +149,16 @@ if __name__ == '__main__':
              'water heaters':(np.arange(1988,2013),[28.07,8.4,5.86,6.37,3.9,4.08,5.42,4.12,3.45,3.31,3.12,1.64,2.36,
                                                     1.8,5.48,1.35,1.47,0.52,1.03,3.28,-1.4,1.72,1.26,0.62,1.25])
              }
-             
+    
+    time1 = time.perf_counter()
+    s = data_set["room air conditioners"][1]
+    est_abm = EstimateABM(s)
+    p0, q0 = est_abm.gener_p0_q0()
+    result = est_abm.solution_search(p0, q0)
+    print(f'    Time elasped: {time.perf_counter()-time1:.2f} s')
+    print(f"    R2:{result['fitness']:.4f}    num_nodes:{result['num_nodes']}")
+
+    '''
     est_dict = {}
     for k in sorted(data_set.keys()):
         print(f'====================================={k}=====================================')
@@ -158,4 +171,4 @@ if __name__ == '__main__':
                        'curve': f_act, 'r2': R2, 'path': pq_set}
         print(f'    Time elasped: {time.perf_counter()-time1:%.2f} s')
         print(f'    R2:{R2:.4f}    steps:{steps:%d}')
-
+    '''
