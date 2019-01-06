e311a26a752d562e35ce0bb039ed1cff7d5954b5
uscxiaoyu <jxndtbxiaoyu@163.com>
2019-01-06 11:29:57 +0800

    u

--- generate_abm_dataset.py
+++ generate_abm_dataset.py
@@ -17,11 +17,7 @@ def generate_random_graph(degre_sequance):
 def func(p, q, g):
     p = round(p, 5)
     q = round(q, 5)
-<<<<<<< HEAD
     diff = Diffuse(p, q, g=g, num_runs=40)
-=======
-    diff = Diffuse(p, q, g=g, num_runs=35)
->>>>>>> eae8033dfffe1026b2124e729e4a261f4250e579
     x = np.mean(diff.repete_diffuse(), axis=0)
     return str([p, q]), list(np.concatenate(([p, q], x)))
 
@@ -37,12 +33,7 @@ if __name__ == '__main__':
     epinions_graph = nx.read_gpickle('dataSources/epinions.gpickle')
     txt_cont = [x['_id'] for x in prj.find({}, projection={'_id': 1})]
 
-<<<<<<< HEAD
     prj2 = db.abmDatabase  # 新建一个集合
-=======
-    prj2 = client.abmDatabase
-    
->>>>>>> eae8033dfffe1026b2124e729e4a261f4250e579
     for i, key in enumerate(txt_cont):
         mongo_date = prj.find_one({"_id": key})
         r_p = mongo_date['param_boundary']['p_range']
