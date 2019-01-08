4feae05c64918e3b29f43d0112d6cde4635bf958
uscxiaoyu <yx_402@usc.edu>
2019-01-08 11:24:53 +0800

    win

--- abm_est_empi_data.py
+++ abm_est_empi_data.py
@@ -15,12 +15,9 @@ def generate_random_graph(degre_sequance):
 def insert_data(g, g_name, key, data_set):
     s = data_set[key][1]
     res = prj.find_one({"_id": g_name}, projection={key: 1})
-    if res:  # 查看有没有插入g_name，如果插入了，则查看是否存在res[key]
-        len_res = len(res.get(key, []))
-    else:
-        len_res = 0
-    while len_res < 10:
-        print(f"第{len_res + 1}轮:")
+    len_key = len(res.get(key, [])) if res else 0  # 第一次查找key，res为None
+    while len_key < 10:
+        print(f"第{len_key + 1}轮:")
         t1 = time.perf_counter()
         est_abm = estimateABM(s, G=g, m_p=True)
         p0, q0 = est_abm.gener_init_pq()
@@ -30,13 +27,15 @@ def insert_data(g, g_name, key, data_set):
         print(f'    p0:{p0:.5f}, q0:{q0:.5f}')
         print(f" R2:{result['fitness']:.4f}    num_nodes:{result['num_nodes']}")
 
-        if len_res == 0:
+        if not res:
             prj.insert_one({"_id": g_name, key: [str(result)]})  # result的字符串形式，为了和MongoDB兼容
+        elif len_key == 0:
+            prj.update_one({"_id": g_name}, {"$set": {key: [str(result)]}})
         else:
             prj.update_one({"_id": g_name}, {"$push": {key: str(result)}})
 
         res = prj.find_one({"_id": g_name}, projection={key: 1})
-        len_res = len(res.get(key, []))
+        len_key = len(res.get(key, []))
     else:
         print(f"  任务已完成！")
 
@@ -66,13 +65,16 @@ if __name__ == '__main__':
                 'lognormal_graph(10000,3)', 'facebook_graph', 'epinions_graph']
 
     for key in ["color televisions", "room air conditioners", "clothers dryers"]:
+        print(key)
         for i, g_name in enumerate(g_name_cont):
             print(i, g_name)
             if g_name in ['exponential_graph(10000,3)', 'gaussian_graph(10000,3)', 'lognormal_graph(10000,3)']:
                 graph = g_name[:5] + "_seq"
-                g = eval(graph)
-            elif g_name in ['facebook_graph', 'epinions_graph']:
-                g = eval(g_name)
+                g = generate_random_graph(eval(graph))
+            elif g_name == 'facebook_graph':
+                g = facebook_graph
+            elif g_name == 'epinions_graph':
+                g = epinions_graph
             else:
                 g = eval("nx." + g_name)
             insert_data(g, g_name_cont[i], key, data_set)
