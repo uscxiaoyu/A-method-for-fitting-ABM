6ee2ae9e555b068ef85dc2f9c3be9989089c4d18
uscxiaoyu <jxndtbxiaoyu@163.com>
2019-03-19 10:24:39 +0800

    mac

--- pip upgrade.py
+++ pip upgrade.py
@@ -1,8 +1,8 @@
 import os
 import time
-raw_dated = os.popen("sudo pip3.6 list --outdated").read()
+raw_dated = os.popen("sudo pip3.7 list --outdated").read()
 outdated = [x.split(' ')[0] for x in raw_dated.split('\n')[2:]]
 for dist in outdated:
     t1 = time.process_time()
-    os.system("sudo pip3.6 install --upgrade " + dist)
+    os.system("sudo pip3.7 install --upgrade " + dist)
     print(f'{dist} 耗时{time.process_time()-t1 : .2f}秒')
